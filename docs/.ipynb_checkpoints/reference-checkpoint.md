cb5cd532f430694ee3bc8be36529378765c58ddd
francis2martinez <39139670+francis2martinez@users.noreply.github.com>
2025-10-31 19:04:55 -0400

    "Remove all .ipynb_checkpoints folders from repo"

--- docs/.ipynb_checkpoints/reference-checkpoint.md
+++
@@ -1,6 +0,0 @@
-# Python package docs
-
-::: InDelsTopo
-    options:
-      show_root_toc_entry: true
-      members: true
--- src/InDelsTopo/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,2 +0,0 @@
-#from .Chain_and_Block import Alphabet, Chain, Block
-#from .Filtration_and_Complex import Filtration, Complex
